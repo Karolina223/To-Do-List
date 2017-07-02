28465e6c9167cd401744b47b2501666a07d93585
Karolina <karolinarys93@gmail.com>
2017-07-02 21:12:05 +0200

    Add files via upload

--- skrypt.js
+++ skrypt.js
@@ -3,9 +3,10 @@ $(function(){
         $('li').remove();
     }); 
     
-    $('.close').click(function(){
-        $(this).parent('li').remove();
-    });
+    $('.form').on('click', '.close', function(){
+    $(this).parent('li').remove();
+    return false;
+});
 })
 
 var addli = document.querySelector(".add");
@@ -16,15 +17,13 @@ addli.addEventListener("click", function() {
 	var list = document.querySelector(".form");
 	var input = document.querySelector(".field");
 	
+	
 	li.innerHTML = input.value + '<span class="close">X</span>';
 	input.value = "";
 	list.appendChild(li);
 	li.className += "add_new";
     
+    
 }, false);
-
-
-
-
 
 
